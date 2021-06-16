0119f3a1686ec5a3a31aa0aa36b40652469d9558
kpaller <kimchesed.paller@analog.com>
2021-06-16 13:33:52 +0800

    install telemetry in  docker

--- web/dashboard/dasher/start_docker.sh
+++ web/dashboard/dasher/start_docker.sh
@@ -1,3 +1,4 @@
+set -e
 IMAGE=$(docker ps -q --filter ancestor="constellation" )
 if [ "$IMAGE" ]
 then
@@ -5,6 +6,13 @@ then
 	docker stop $IMAGE
 	docker rm $IMAGE
 fi
+
+[ -d "telemetry_src" ] && rm -rf "telemetry_src"
+OLD_PATH=`pwd`
+cd ../../../../.
+cp -r telemetry telemetry_src
+mv telemetry_src telemetry/web/dashboard/dasher/
+cd $OLD_PATH
 
 docker build -t constellation .
 docker run -d --restart unless-stopped -p 5000:5000 constellation
